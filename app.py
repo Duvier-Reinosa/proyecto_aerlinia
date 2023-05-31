03a13ee905ddbdfeb7e16206fffb35d51c03d64a
Duvier-Reinosa <duvierreinosa@gmail.com>
2023-05-30 22:54:39 -0500

    añadí carpetas de metodos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,16 +8,24 @@ def index():
     return render_template('vistas/index.html')
 
 @app.route('/vuelo')
-def index():
+def vuelo():
     return render_template('vistas/vuelo.html')
 
 @app.route('/misvuelos')
-def index():
-    return render_template('vistas/index.html')
+def misVuelos():
+    return render_template('vistas/misVuelos.html')
 
 @app.route('/pagar')
-def index():
-    return render_template('vistas/index.html')
+def pagar():
+    return render_template('vistas/pagar.html')
+
+@app.route('/listaVuelos', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        # agregar metodo para obtener los vuelos, se pueden guardar en un archivo de texto hacer un metodo en el archivo metodos para obtener los vuelos
+        return []
+    else:
+        return []
 
 if __name__ == '__main__':
     app.run()
