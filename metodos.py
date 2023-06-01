e85d7237488f663e56ec55af9a3c7c8a0c045e1d
angola031 <c.angola@utp.edu.co>
2023-06-01 17:20:28 -0500

    actualizacion metodos

--- metodos.py
+++ metodos.py
@@ -60,9 +60,10 @@ def leer_vuelos():
         
 #-----------verificaciones--------------
 def verificar_matricula(aviones,matricula):
+    if os.path.exists("archivos/aviones.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        aviones=leer_usuarios()
     ver=True
     while ver:
-        matricula=input("Digite la matricula: ")
         cant_aviones=len(aviones)
         con_avi=0
         if cant_aviones>0:
@@ -78,24 +79,29 @@ def verificar_matricula(aviones,matricula):
             
     return matricula
 def verificar_identificacion(usuarios,cedula):
+    if os.path.exists("archivos/usuario.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        usuarios=leer_usuarios()
     ver=True
     while ver:
-        cedula=input("Digite la identificacion: ")
+        
         cant_usuarios=len(usuarios)
         con_usu=0
         if cant_usuarios>0:
             for c in usuarios:
                 if c.identifica==cedula:
-                    print("La cedula ya existe")
-                    break
+                    print("El usuario ya esta registrado")
+                    return False
                 con_usu+=1
                 if con_usu==cant_usuarios:
                     ver=False
+                    return True
         else:
             ver=False
+            return False
             
-    return cedula 
 def verificar_vuelo(vuelos,codigo):
+    if os.path.exists("archivos/vuelos.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        vuelos=leer_usuarios()
     ver=True
     while ver:
         codigo=input("Digite el codigo: ")
@@ -113,36 +119,14 @@ def verificar_vuelo(vuelos,codigo):
             ver=False
             
     return codigo
-def verificar_tiquete(tiquetes,codigo):
-    ver=True
-    while ver:
-        codigo=input("Digite el codigo: ")
-        cant_tiquetes=len(tiquetes)
-        con_tiquete=0
-        if cant_tiquetes>0:
-            for c in tiquetes:
-                if c.codigo==codigo:
-                    print("El codigo ya existe")
-                    break
-                con_tiquete+=1
-                if con_tiquete==cant_tiquetes:
-                    ver=False
-        else:
-            ver=False
-            
-    return codigo
+
 
 
 def registrar_aviones(aviones,matricula,modelo,marca,anio,aerolinea,horas_vuelo,capacida_silla):
     if os.path.exists("archivos/aviones.pickle"): ### reviso si el archivo esta creado para obtener los datos
         aviones=leer_aviones()
-    matricula=verificar_matricula(aviones,matricula)         
-    modelo=input("Modelo avion: ")
-    marca=input ("Marca del avion: ")
-    anio= input("Año del avion: ")
-    aerolinea=input("Aerolinea: ")
-    horas_vuelo=input("Horas de vuelo: ")
-    capacida_silla=input("Capacidad de sillas: ")
+    matric=verificar_matricula(aviones,matricula)         
+    
     avion= clases.Avion(matricula,modelo,marca,anio,aerolinea,horas_vuelo,capacida_silla)
     aviones.append(avion)
     registra_avion(aviones)
@@ -150,11 +134,6 @@ def registrar_aviones(aviones,matricula,modelo,marca,anio,aerolinea,horas_vuelo,
 def registrar_usuarios(usuarios,identificacion,nombre,celular,correo,contrasena,millas):
     if os.path.exists("archivos/usuario.pickle"): ### reviso si el archivo esta creado para obtener los datos
         usuarios=leer_usuarios()
-    identificacion=verificar_identificacion(usuarios,identificacion)         
-    nombre=input("Nombre: ")
-    celular=input ("celular: ")
-    correo= input("correo: ")
-    contrasena=input("contraseña: ")
     usuario= clases.Pasajero(identificacion,nombre,celular,correo,contrasena,millas)
     usuarios.append(usuario)
     registra_usuario(usuarios)
@@ -162,47 +141,124 @@ def registrar_usuarios(usuarios,identificacion,nombre,celular,correo,contrasena,
 
 def registrar_funcionario(funcionario,identificacion,nombre,edad,celular,correo,contrasena,funcion):
     if os.path.exists("archivos/funcionario.pickle"): ### reviso si el archivo esta creado para obtener los datos
-        funcionario=leer_funcionario()
-    identificacion=verificar_identificacion(funcionario,identificacion)         
-    nombre=input("Nombre: ")
-    celular=input ("celular: ")
-    correo= input("correo: ")
-    contrasena=input("contraseña: ")
-    funcion=input("funcion: ")
-    
+        funcionario=leer_funcionario()        
     funcionari= clases.Tripulacion(identificacion,nombre,edad,celular,correo,contrasena,funcion)
     funcionario.append(funcionari)
     registra_funcionario(funcionario)
 
 def registrar_piloto(piloto,identificacion,nombre,celular,edad,correo,contrasena,rango,licencia,hora_vuelo):
     if os.path.exists("archivos/pilotos.pickle"): ### reviso si el archivo esta creado para obtener los datos
-        piloto=leer_pilotos()
-    identificacion=verificar_identificacion(piloto,identificacion)         
-    nombre=input("Nombre: ")
-    edad=input ("edad: ")
-    correo= input("correo: ")
-    contrasena=input("contraseña: ")
-    rango=input("rango: ")
-    hora_vuelo=input("hora de vuelo: ")
+        piloto=leer_pilotos()     
     pilot= clases.Piloto(identificacion,nombre,celular,correo,contrasena,edad,licencia,rango,hora_vuelo)
     piloto.append(pilot)
     registra_pilotos(piloto)
 
-def registrar_vuelos(vuelos,codigo,origen,destino,fecha,hora,valor,avion):
+def registrar_vuelos(vuelos,codigo,tipo_vuelo,tipo_tarifa,origen,destino,fecha,hora_ida,hora_llegada,valor_vuelo):
     if os.path.exists("archivos/vuelos.pickle"): ### reviso si el archivo esta creado para obtener los datos
         vuelos=leer_vuelos()
-    codigo=input("codigo: ")       
-    origen=input("origen: ")
-    destino=input ("destino: ")
-    fecha= input("fecha: ")
-    hora=input("hora: ")
-    valor=input("valor: ")
-    avion=input("avion: ")
-    vuelo= clases.Vuelo(codigo,origen,destino,fecha,hora,valor,avion)
+    vuelo= clases.Vuelo(codigo,tipo_vuelo,tipo_tarifa,valor_vuelo,origen,destino,fecha,hora_ida,hora_llegada)
     vuelos.append(vuelo)
     registra_vuelos(vuelos)
-#registrar_vuelos([],None,None,None,None,None,None,None)
-#registrar_piloto([],None,None,None,None,None,None,None,None,None)
+
+
+##---consultar vuelos----
+def consultar_vuelos(vuelos,c_origen,c_destino,fecha):
+    if os.path.exists("archivos/vuelos.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        vuelos=leer_vuelos()
+    cant_vuelos=len(vuelos)
+    cont_vuelos=0
+    for vuel in vuelos:
+        if vuel.origen==c_origen and vuel.destino==c_destino and vuel.fecha_ida==fecha:
+            return True
+        cont_vuelos+=1
+        if cant_vuelos==cont_vuelos:
+            return False
+
+def registrar_tiquetes(tiquetes,codigo,tipo_vuelo,tipo_tarifa,c_origen,c_destino,fecha,hora_ida,hora_llegada)  :
+    pass     
+##----------verificar usuario--------
+def verificar_ingreso_usuario(usuarios,identificacion,contraseña):
+    if os.path.exists("archivos/usuario.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        usuarios=leer_usuarios()
+    ver=True
+    while ver:
+        identificacion=input("Digite la identificacion: ")
+        contraseña=input("contraseña ")
+        cant_usuarios=len(usuarios)
+        con_usu=0
+        if cant_usuarios>0:
+            for c in usuarios:
+                if c.identifica==identificacion and contraseña==c.contrasena:
+                    ver=False
+                    return True
+                    break
+                con_usu+=1
+                if con_usu==cant_usuarios:
+                    print("La identificacion o contraseña erronea")
+                    return False
+                    break
+        else:
+            print("No hay usuarios registrados")
+            return False
+            break
+            
+##----------verificar funcionario--------
+def verificar_ingreso_funcionario(funcionario,identificacion,contraseña):
+    if os.path.exists("archivos/funcionario.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        funcionario=leer_funcionario()
+    ver=True
+    while ver:
+        cant_funcionario=len(funcionario)
+        con_funcionario=0
+        if cant_funcionario>0:
+            for c in funcionario:
+                if c.identifica==identificacion and contraseña==c.contrasena:
+                    ver=False
+                    return True
+                    break
+                con_funcionario+=1
+                if con_funcionario==cant_funcionario:
+                    print("La identificacion o contraseña erronea")
+                    return False
+                    break
+        else:
+            print("No hay funcionario registrados")
+            return False
+            break
+
+##----------verificar piloto--------
+def verificar_ingreso_piloto(piloto,identificacion,contraseña):
+    if os.path.exists("archivos/pilotos.pickle"): ### reviso si el archivo esta creado para obtener los datos
+        piloto=leer_pilotos()
+    ver=True
+    while ver:
+        cant_piloto=len(piloto)
+        con_piloto=0
+        if cant_piloto>0:
+            for c in piloto:
+                if c.identifica==identificacion and contraseña==c.contrasena:
+                    ver=False
+                    return True
+                    break
+                con_piloto+=1
+                if con_piloto==cant_piloto:
+                    print("La identificacion o contraseña erronea")
+                    return False
+                    break
+        else:
+            print("No hay piloto registrados")
+            return False
+            break
+
+
+##----------verificar vuelos  programados---
+
+
+registrar_vuelos([],"1","comercial",["viajero","preferencia","premium"],"perira","cali","15-7-2023","13:00","15:00",[200.000,300.000,400,000])
+registrar_piloto([],"11182","carlo angola","31233564","25","agola@sa.com","32545","comandante","32sa","300")
 #registrar_funcionario([],None,None,None,None,None,None,None)
-##registrar_usuarios([],None,None,None,None,None,None)
+#registrar_usuarios([],None,None,None,None,None,None)
+print(consultar_vuelos([],"bogota","cali","12-6-2023"))
+registrar_vuelos([],None,None,None,None,None,None,None,None)
+#print(verificar_ingreso_usuario([],None,None))
 #registrar_aviones([],None,None,None,None,None,None,None)
